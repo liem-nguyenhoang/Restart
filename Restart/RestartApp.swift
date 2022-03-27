fd9c2faa96983aa7c532a492fbb6d555c17533e2
LiemNH <liem.nguyehoang.127@gmail.com>
2022-03-27 11:07:08 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by LiemNH2 on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
