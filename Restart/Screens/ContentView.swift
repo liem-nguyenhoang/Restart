e123f62f4572204a2a6bc4948feae506d31e7ba2
LiemNH <liem.nguyehoang.127@gmail.com>
2022-03-28 21:19:45 +0700

    [LiemNH] Complete code

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by LiemNH2 on 27/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack{
+            if isOnboardingViewActive{
+                OnBoardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
